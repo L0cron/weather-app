58f2e942ad92a07c3f34ff146b942fefca256518
Горбач Александр <25.Gorbach.A@school548.ru>
2024-02-05 18:36:20 +0300

    1123

--- serverside/city_maker.py
+++ serverside/city_maker.py
@@ -7,7 +7,7 @@ import ast
 import time
 
 
-cities = pd.read_csv("./serverside/cities.csv", index_col=0)
+cities = pd.read_csv("./cities.csv", index_col=0)
 
 
 
@@ -43,9 +43,30 @@ print(rlatitude[0], rlongitude[0])
 
 all_cities = []
 
+api = '32442b1be417871ff0af3e7e5cb87db8'
+def get_city(city_req, recur=1):
+    try:
+        r2 = requests.get('https://api.openweathermap.org/data/2.5/weather?q='+city_req+'&units=metric&lang=ru&appid='+api, timeout=240)
+        print("content is " + r2.content.decode("UTF-8"))
+        
+        result = ast.literal_eval(r2.content.decode("UTF-8"))
+
+        if 'message' in result:
+            print("City not found " + city)
+
+        return result
+    except requests.ReadTimeout:
+        print("idk - recursim " + str(recur))
+        recur += 1
+        get_city(city_req, recur)
+    except requests.exceptions.ConnectionError:
+        print("idk - recursim " + str(recur))
+        recur += 1
+        get_city(city_req, recur)
+
 for j in range(len(rlatitude)):
     geo = {'format': 'json', 'lat': f'{rlatitude[j]}', 'lon': f'{rlongitude[j]}'}
-    r = requests.get('https://nominatim.openstreetmap.org/reverse', params=geo)
+    r = requests.get('https://nominatim.openstreetmap.org/reverse', params=geo, timeout=240)
 
 
 
@@ -58,8 +79,9 @@ for j in range(len(rlatitude)):
     
     city_req = city.replace(' ', '&')
     print(city_req)
-    r2 = requests.get('https://api.openweathermap.org/data/2.5/weather?q='+city_req+'&units=metric&lang=ru&appid=79d1ca96933b0328e1c7e3e7a26cb347')
-    print(r2.content)
+
+    result = get_city(city_req)
+    print(result)
     # all_cities.append(city)
     # print("timed out: " + city)
     # print("City not found: " + city)
